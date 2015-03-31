cd9d249c39c2f03471fa069f6ed264b6425e8504
suwilliam <williamsu@gatech.edu>
2015-03-31 19:49:47 -0400

    changed testing code

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 from numpy import *
-def readingDAT(filename):
+def readingMatrixDAT(filename):
 	matrixList = []
 	matrixList2 = []
 	for line in open(filename, 'r'):
@@ -9,16 +9,23 @@ def readingDAT(filename):
 	    	aList[num] = float(aList[num])
 	    matrixList.append(aList)
 	mat = matrix(matrixList)
-	print mat
-	if mat.shape[0] == mat.shape[1]:
-		return mat
-	else:
-		b = mat[:,(mat.shape[1]-1)].tolist()
-		for line in matrixList:
-			newList = []
-			for x in range(len(line)-1):
-				newList.append(line[x])
-			matrixList2.append(newList)
-		mat = matrix(matrixList2)
-		return mat, b
-print readingDAT("b.dat")
+	return mat
+def readingAugmentDAT(filename):
+	matrixList = []
+	matrixList2 = []
+	for line in open(filename, 'r'):
+	    item = line.rstrip()
+	    aList = item.split(" ")
+	    for num in range(len(aList)):
+	    	aList[num] = float(aList[num])
+	    matrixList.append(aList)
+	mat = matrix(matrixList)
+	b = mat[:,(mat.shape[1]-1)].tolist()
+	for line in matrixList:
+		newList = []
+		for x in range(len(line)-1):
+			newList.append(line[x])
+		matrixList2.append(newList)
+	mat = matrix(matrixList2)
+	return mat, b
+print readingAugmentDAT("b.dat")
