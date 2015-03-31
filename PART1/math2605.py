831520f2906ad7fd377aa67a6ddbb10b253ede60
Alex Kim <akim316@gatech.edu>
2015-03-30 21:19:43 -0400

    solve_qr_b added

--- PART1/math2605.py
+++ PART1/math2605.py
@@ -3,8 +3,15 @@ from numpy import *
 mat = matrix([[10,22,39],[49,81,21],[1,-11,35]])
 lower = matrix([[1, 0, 0, 0], [0.5, 1, 0, 0], [0.3333333333333, 1, 1, 0], [0.25, 9, 1.5, 1]])
 upper = matrix([[1,1,1,1],[0,1,1,1],[0,0,1,1],[0,0,0,1]])
-be = matrix([[2],[2],[2],[2]])
-
+be = matrix([[0.0464159],[0.0464159],[0.0464159],[0.0464159]])
+Q = matrix([[0.838116, -0.522648, 0.153973, 0.0263067],
+            [0.419058, 0.441713, -0.727754, -0.31568],
+            [0.279372, 0.528821, 0.139506, 0.7892],
+            [0.209529, 0.502072, 0.653609, -0.526134]])
+R = matrix([[1.19315, 0.670493, 0.474933, 0.369835],
+            [0, 0.118533, 0.125655, 0.117542],
+            [0, 0, 0.00622177, 0.00956609],
+            [0, 0, 0, -0.000187905]])
 def matrixMult(A,B):
     D = zeros((A.shape[0],B.shape[1]))
     for x in range(A.shape[0]):
@@ -58,6 +65,22 @@ def solve_lu_b(L, U, b):
                     x[i,0] = (y[i,0] - added) / current
                     break
     return x
+def solve_qr_b(Q, R, b):
+    x = zeros((R.shape[0], 1))
+    for i in xrange(R.shape[0]-1, -1, -1):
+        added = 0
+        current = 0
+        for j in xrange (R.shape[1]-1, -1, -1):
+            if i == R.shape[0]-1:
+                x[i,0] = b[i,0] / R[R.shape[0]-1,R.shape[1]-1]
+            else:
+                if j > i:
+                    added = R[i,j]*x[j,0] + added
+                elif j == i:
+                    current = R[i,j]
+                    x[i,0] = (b[i,0] - added) / current
+                    break
+    return x   
 def power_method_calculations(A, tol, initEig, iters):
     ## Ax0
     result = matrixMult(A, initEig)
@@ -71,4 +94,4 @@ def power_method_calculations(A, tol, initEig, iters):
     else:
         return iters, norm(max(result)), nextEig
 
-print solve_lu_b(lower, upper, be)
+print solve_qr_b(Q, R, be)
